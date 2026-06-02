ce75338fb39346d2baef675f344140a74741cbf0
Kiran Anand <liteninkiran@hotmail.com>
2026-06-02 19:53:58 +0100

    005 - Skipping Tests and Running Only a Few Tests

--- src/app/common/calculator.spec.ts
+++ src/app/common/calculator.spec.ts
@@ -1,9 +1,23 @@
 import { describe, it, expect } from 'vitest';
 import { calculator } from './calculator';
 
-describe('Vitest Fundamentals', () => {
-  it('should add two numbers', () => {
+const describeIf = describe.skipIf(true);
+
+const fundamentalsTests = () => {
+  it.only('should add two numbers', () => {
     const result = calculator.add(2, 3);
     expect(result).toBe(5);
   });
-});
+
+  it.skip('should add two numbers v2', () => {
+    const result = calculator.add(2, 3);
+    expect(result).toBe(5);
+  });
+
+  it('should add two numbers v3', () => {
+    const result = calculator.add(2, 3);
+    expect(result).toBe(5);
+  });
+};
+
+describeIf('Vitest Fundamentals', fundamentalsTests);
