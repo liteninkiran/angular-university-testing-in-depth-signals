cb90b6b856d48fe82de2261094e009f8b7174ff4
Kiran Anand <liteninkiran@hotmail.com>
2026-06-02 20:08:52 +0100

    007 - Understanding Mocking in Vitest

--- src/app/common/calculator.spec.ts
+++ src/app/common/calculator.spec.ts
@@ -14,9 +14,20 @@ const itShowsHowSpiesWork = () => {
   expect(spy).toHaveBeenCalledWith(2, 3);
 };
 
+const itShowsHowMockingWorks = () => {
+  const spy = vi.spyOn(calculator, 'add').mockReturnValue(5);
+  const result = calculator.add(2, 3);
+  expect(result).toBe(5);
+  expect(spy).toHaveBeenCalledOnce();
+  expect(spy).toHaveBeenCalledWith(2, 3);
+  const result2 = calculator.add(5, 5);
+  expect(result2).toBe(5);
+};
+
 const fundamentalsTests = () => {
-  it.skip('should add two numbers', itShouldAddTwoNumbers);
+  it('should add two numbers', itShouldAddTwoNumbers);
   it('shows how spies work', itShowsHowSpiesWork);
+  it.only('shows how mocking works', itShowsHowMockingWorks);
 };
 
 describe('Vitest Fundamentals', fundamentalsTests);
