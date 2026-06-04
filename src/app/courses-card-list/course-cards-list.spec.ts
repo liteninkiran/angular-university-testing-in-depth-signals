291639ddae7e4fcf287b1e297ddb737cb7ce5530
Kiran Anand <liteninkiran@hotmail.com>
2026-06-04 15:57:21 +0100

    023 - Courses Cards List - Testing if All Cards are Displayed

--- src/app/courses-card-list/course-cards-list.spec.ts
+++ src/app/courses-card-list/course-cards-list.spec.ts
@@ -1,13 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MOCK_COURSES } from '../testing/testing-data';
-import { describe, it, beforeEach } from 'vitest';
+import { describe, expect, it, beforeEach } from 'vitest';
 import { provideRouter } from '@angular/router';
+import { By } from '@angular/platform-browser';
 import { CoursesCardList } from './courses-card-list';
 import { CoursesDialog } from '../courses-dialog/courses-dialog';
+import { DebugElement } from '@angular/core';
 
 describe('CoursesCardList', () => {
   let component: CoursesCardList;
   let fixture: ComponentFixture<CoursesCardList>;
+  let de: DebugElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -17,10 +20,16 @@ describe('CoursesCardList', () => {
 
     fixture = TestBed.createComponent(CoursesCardList);
     component = fixture.componentInstance;
+    de = fixture.debugElement;
 
     fixture.componentRef.setInput('courses', MOCK_COURSES);
     fixture.detectChanges();
   });
 
-  it('should display the course list', () => {});
+  it('should display the course list', () => {
+    const cardTitles = de.queryAll(By.css('.course-card .card-header'));
+    expect(cardTitles.length).toBe(2);
+    const titleEl = cardTitles[0].nativeElement;
+    expect(titleEl.textContent).toBe('Beginner Course');
+  });
 });
